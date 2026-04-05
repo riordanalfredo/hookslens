9d34cd21cbd89174180629faa505fd24df3f1642
riordanalfredo <riordanalfredo@users.noreply.github.com>
2026-04-05 00:35:57 +0000

    deploy: e29e9bca48d7399013e9b6866a9d846ae5eacd6f

--- demo/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import HooksLensDemo from "./hookslens-demo.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <HooksLensDemo />
-  </React.StrictMode>,
-);
--- src/global.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "*.css";
-
-declare const process: {
-  env: {
-    NODE_ENV?: "development" | "production" | "test" | string;
-    [key: string]: string | undefined;
-  };
-};
