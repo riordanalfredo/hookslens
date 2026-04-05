001608bc6030a2b6f8f01accc51e1323e29b4c61
Riordan Alfredo <riordan.alfredo@gmail.com>
2026-04-05 10:44:52 +1000

    fix: add base config for demo

--- demo/vite.config.mjs
+++ demo/vite.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/hookslens/", // Uncomment this for production deployment
   root: "demo",
   plugins: [react()],
   build: {
