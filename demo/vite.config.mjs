1a47df37aad99aded7c7ee9640f374cd01b92b73
Riordan Alfredo <riordan.alfredo@gmail.com>
2026-04-05 10:23:55 +1000

    chore: add deployment stuff for demo

--- demo/vite.config.mjs
+++ demo/vite.config.mjs
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   root: "demo",
   plugins: [react()],
+  build: {
+    outDir: "../dist/demo",
+    emptyOutDir: true,
+  },
   server: {
     port: 5173,
   },
